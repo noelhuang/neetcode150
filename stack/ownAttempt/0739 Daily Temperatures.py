f3b434c22835b2e2661fecaf24fb3fc38c49765d
Noël Huang <Noel.Huang@cohesivegroup.com>
2023-07-27 06:35:29 +0000

    add own attempt 0739

---
+++ stack/ownAttempt/0739 Daily Temperatures.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        my_deque = deque()
+        my_array = [0 for i in range(len(temperatures))]
+        for i, temp in enumerate(temperatures):
+            if len(my_deque) == 0:
+                my_deque.append((i, temp))
+            else:
+                flag = True
+                while len(my_deque) > 0 and flag:
+                    top_value = my_deque[-1]
+                    if temp > top_value[1]:
+                        delta_index = i - top_value[0]
+                        my_array[top_value[0]] = delta_index
+                        my_deque.pop()
+                    else:
+                        flag = False
+                my_deque.append((i, temp))
+        return my_array
+
+"""
+Explanation:
+Iterate over the temperatures. Initialize stack, which will contain (index, temperature) tuple.
+Compare temperature to top value of stack. If none, append to stack including the index. 
+If temperature > popped value of stack, calculate delta in index. THe delta index should be added in an array at the 
+index of the popped value. 
+"""
